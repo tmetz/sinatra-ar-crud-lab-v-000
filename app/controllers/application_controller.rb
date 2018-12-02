88aa79dd7cd9993afed335eb45325a65b87652c5
Tammy Metz <tammy@tammymetz.com>
2018-12-02 15:06:00 +0000

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,3 @@
-
 require_relative '../../config/environment'
 
 class ApplicationController < Sinatra::Base
@@ -9,5 +8,39 @@ class ApplicationController < Sinatra::Base
   end
 
   get '/' do
+  end
+
+  get '/articles' do
+    @articles = Article.all
+    erb :index
+  end
+
+  post '/articles' do
+    article = Article.create(:title => params[:title], :content => params[:content])
+    redirect ("/articles/#{article.id}")
+  end
+
+  get '/articles/new' do
+    erb :new
+  end
+
+  get '/articles/:id' do
+    @article = Article.find(params[:id])
+    erb :show
+  end
+
+  patch '/articles/:id' do
+    Article.find(params[:id]).update(:title => params[:title], :content => params[:content])
+    redirect ("/articles/#{params[:id]}")
+  end
+
+  delete '/articles/:id/delete' do
+    Article.find(params[:id]).destroy
+    redirect ("/")
+  end
+
+  get '/articles/:id/edit' do
+    @article = Article.find(params[:id])
+    erb :edit
   end
 end
--- db/migrate/20151201201114_create_articles.rb
+++ db/migrate/20151201201114_create_articles.rb
@@ -1,9 +1,9 @@
 class CreateArticles < ActiveRecord::Migration[5.1]
   def change
+    Article.reset_column_information
     create_table :articles do |t|
       t.string :title
       t.string :content
-
     end
   end
 end
